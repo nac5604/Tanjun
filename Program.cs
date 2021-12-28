a3cbe52cf6df5e4fb953e45e9b24217ba3dad24c
TREE\GRAB <85867267+nac5604@users.noreply.github.com>
2021-12-28 17:31:56 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tanjun
+{
+    static class Program
+    {
+        // Entry Point
+
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
+        // Global Variables
+        public static List<string> code = new List<string>();
+        public static TanjunProject currentProject = new TanjunProject();
+        public static string currentFile;
+    }
+}
